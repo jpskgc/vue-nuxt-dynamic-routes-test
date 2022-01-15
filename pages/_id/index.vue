d7c85ac7b359ee561946ea667ed4c64ef163975b
jpskgc <jpskgc.v3@gmail.com>
2022-01-15 17:26:53 +0900

    first commit

---
+++ pages/_id/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>test</div>
+    <div>{{ id }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class Page extends Vue {
+  id = this.$route.params.id
+}
+</script>
